4d77078a41958217b6d31601f46a9990587be60c
Kritika1206 <77856536+Kritika1206@users.noreply.github.com>
2023-06-19 14:27:40 -0400

    Add files via upload

---
+++ CLE1.cpp
@@ -0,0 +1,15 @@
+/* Author: Kritika Singh
+ Name of Program: Computer Programming
+ Student Number: 100859600
+ Date: May 5, 2022
+ This program is designed to print my name, course name and one of my favourite quote.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello there! My name is Kritika Singh." << endl;
+    cout << "I am enrolled in Computer Programming and this is OOP-2200-06\n" ;
+    cout << "The longer I live, the more beautiful life becomes. — Frank Lloyd Wright 66." << endl;
+}
